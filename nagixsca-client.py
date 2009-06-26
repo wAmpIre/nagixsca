759e81b49bf65f98368a4ba65b84ebf2cfca76b5
Sven Velt <sven@velt.de>
2009-06-26 18:58:28 +0200

    (Untested) Encoding, time checks
    Added encoding to both sides. At the moment we have "base64" (default)
    and "plain" (not RECOMMENDED! May f..k up XML file). You can choose one
    with "-e (base64|plain)" on nagixsca-client.py call
    
    nagixsca-nagios.py now looks to the timestamp in the XML file. If it's
    older than 14400 seconds (4 hours) it adds a text at the beginning of
    the plugin output. Seconds can be changed by "-s 14400". If you
    additionally put "-m" on the command line every outdated check is set to
    UNKNOWN state in Nagios.

--- nagixsca-client.py
+++ nagixsca-client.py
@@ -6,30 +6,54 @@ import libxml2
 import optparse
 import re
 import subprocess
+import sys
 
 services = []
+available_encodings = ['base64', 'plain',]
+
 
 parser = optparse.OptionParser()
 
 parser.add_option('-c', '', dest='cfgfile', help='Config file')
 parser.add_option('-o', '', dest='xmlfile', help='Output file')
+parser.add_option('-e', '', dest='encoding', help='Encoding (ATM only "base64")')
 parser.add_option('-v', '', action='count', dest='verb', help='Verbose output')
 
 parser.set_defaults(cfgfile='nagixsca-client.conf')
 parser.set_defaults(xmlfile='nagixsca.xml')
+parser.set_defaults(encoding='base64')
 parser.set_defaults(verb=0)
 
 (options, args) = parser.parse_args()
 
+if options.encoding not in available_encodings:
+	print 'Wrong encoding method "%s"!' % options.encoding
+	print 'Could be one of: %s' % ', '.join(available_encodings)
+	sys.exit(127)
+
 ##############################################################################
 
-file = open(options.cfgfile)
+def encode(data):
+	if options.encoding == 'plain':
+		return data
+	else:
+		return base64.b64encode(data)
+
+##############################################################################
+
+try:
+	file = open(options.cfgfile)
+except IOError:
+	print 'Config file "%s" does not exist!' % options.cfgfile
+	sys.exit(127)
+
 for line in file:
 	result = re.match('^command\[([-_a-zA-Z0-9]+)\]=(.*)$', line)
 	if result:
 		services.append([result.group(1), result.group(2)])
 
-if options.verb >= 1:
+if options.verb >= 3:
+	print 'Python list of services:'
 	print services
 
 xmldoc = libxml2.newDoc('1.0')
@@ -39,17 +63,17 @@ xmltimestamp = xmlroot.newChild(None, 'timestamp', datetime.datetime.now().strft
 for service, cmdline in services:
 
 	cmd = subprocess.Popen(cmdline.split(' '), stdout=subprocess.PIPE)
-	output = cmd.communicate()[0]
+	output = cmd.communicate()[0].rstrip()
 	retcode = cmd.returncode
 
 	if options.verb >= 1:
-		print "\n\n%s\n== %s\n== %s" % (cmdline, output, retcode)
+		print "\nCommand line: %s\n+--> Output: %s\n+--> Returncode: %s" % (cmdline, output.rstrip(), retcode)
 
 	xmlchild = xmlroot.newChild(None, 'service', None)
-	xmlname = xmlchild.newChild(None, 'name', base64.b64encode(service))
-	xmlname.setProp('encoding', 'base64')
+	xmlname = xmlchild.newChild(None, 'name', encode(service))
+	xmlname.setProp('encoding', options.encoding)
 	xmlreturncode = xmlchild.newChild(None, 'returncode', str(retcode))
-	xmloutput = xmlchild.newChild(None, 'output', base64.b64encode(output))
-	xmloutput.setProp('encoding', 'base64')
+	xmloutput = xmlchild.newChild(None, 'output', encode(output))
+	xmloutput.setProp('encoding', options.encoding)
 
 xmldoc.saveFile(options.xmlfile)
--- nagixsca-nagios.py
+++ nagixsca-nagios.py
@@ -12,12 +12,15 @@ parser.add_option('-u', '', dest='url', help='URL of status file (nagixsca.xml)'
 parser.add_option('-f', '', dest='file', help='(Path and) file name of status file')
 parser.add_option('-n', '', dest='name', help='HostName in Nagios')
 parser.add_option('-p', '', dest='pipe', help='Full path to nagios.cmd')
+parser.add_option('-s', '', dest='seconds', type='int', help='Maximum age in seconds of nagixsca.xml timestamp')
+parser.add_option('-m', '', action='store_true', dest='markold', help='Mark (Set state) of too old checks as UNKNOWN')
 parser.add_option('-v', '', action='count', dest='verb', help='Verbose output')
 
 parser.set_defaults(url=None)
 parser.set_defaults(file='nagixsca.xml')
 parser.set_defaults(name=None)
 parser.set_defaults(pipe=None)
+parser.set_defaults(seconds=14400)
 parser.set_defaults(verb=0)
 
 (options, args) = parser.parse_args()
@@ -32,8 +35,17 @@ if options.pipe==None and options.verb <= 1:
 
 ##############################################################################
 
+def decode(data, encoding):
+	if encoding == 'plain':
+		return data
+	else:
+		return base64.b64decode(data)
+
+##############################################################################
+
 now = datetime.datetime.now().strftime('%s')
 commands = ""
+tooold = False
 
 # Open pipe only if not in debugging mode
 if options.verb <= 2:
@@ -50,13 +62,34 @@ else:
 	doc = libxml2.parseFile(options.file)
 
 # Start XML work
-ctxt = doc.xpathNewContext()
-services = ctxt.xpathEval("//service")
+try:
+	timestamp = doc.xpathNewContext().xpathEval('/nagixsca/timestamp')[0].get_content()
+except:
+	print 'No timestamp found in XML file, exiting because of invalid XML data...'
+	sys.exit(127)
+
+timedelta = int(now) - int(timestamp)
+if options.verb >= 2:
+	print 'Age of XML checks: %s secods, max allowed: %s seconds' % (timedelta, options.seconds)
+if timedelta > options.seconds:
+	if options.verb >= 2:
+		print 'Checks are too old (Delta: %s secods)' % timedelta
+	tooold = True
+
+services = doc.xpathNewContext().xpathEval("/nagixsca/service")
 
 for service in services:
-	srvdescr = base64.b64decode(service.xpathEval('name')[0].get_content())
+	xmlname   = service.xpathEval('name')[0]
+	xmloutput = service.xpathEval('output')[0]
+
+	srvdescr = decode(xmlname.get_content(), xmlname.prop('encoding'))
 	retcode  = service.xpathEval('returncode')[0].get_content()
-	output   = base64.b64decode(service.xpathEval('output')[0].get_content()).rstrip()
+	output   = decode(xmloutput.get_content(), xmlname.prop('encoding')).rstrip()
+
+	if tooold:
+		output = 'Nag(ix)SCA: Check result is %s(>%s) seconds old  - %s' % (timedelta, options.seconds, output)
+		if options.markold:
+			retcode = 3  # Unknown
 
 	line = '[%s] PROCESS_SERVICE_CHECK_RESULT;%s;%s;%s;%s' % (now, options.name, srvdescr, retcode, output )
 
